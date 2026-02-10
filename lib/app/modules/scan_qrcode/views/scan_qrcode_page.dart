15bba73c7281a0a293d0f345069b6cd57b1f11ef
chazhang68 <chazhang68@gmail.com>
2026-02-10 20:45:47 +0800

    111

--- lib/app/modules/scan_qrcode/views/scan_qrcode_page.dart
+++ lib/app/modules/scan_qrcode/views/scan_qrcode_page.dart
@@ -28,46 +28,13 @@ class ScanQrcodePage extends GetView<ScanQrcodeController> {
             onDetect: controller.onDetect,
           ),
 
-          // 半透明遮罩 + 扫描框
-          _ScanOverlay(scanSize: 250),
-
-          // 扫描线动画
-          const Center(
-            child: SizedBox(
-              width: 250,
-              height: 250,
-              child: _ScanLineAnimation(),
-            ),
-          ),
-
-          // 扫描框四角装饰
-          Center(
-            child: SizedBox(
-              width: 250,
-              height: 250,
-              child: CustomPaint(
-                painter: _CornerPainter(
-                  color: const Color(0xFF00C853),
-                  cornerLength: 20,
-                  cornerWidth: 3,
-                ),
-              ),
-            ),
-          ),
-
-          // 提示文字
+          // 扫描线动画（宽度全屏，高度避开顶部AppBar和底部控件）
           Positioned(
-            top: MediaQuery.of(context).size.height / 2 + 145,
+            top: MediaQuery.of(context).padding.top + kToolbarHeight,
+            bottom: 300,
             left: 0,
             right: 0,
-            child: const Text(
-              '将二维码放入框内，即可自动扫描',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 13,
-              ),
-            ),
+            child: const _ScanLineAnimation(),
           ),
 
           // 手电筒按钮（扫描框下方居中）
@@ -195,96 +162,7 @@ class ScanQrcodePage extends GetView<ScanQrcodeController> {
   }
 }
 
-/// 半透明遮罩，中间扫描框区域透明
-class _ScanOverlay extends StatelessWidget {
-  final double scanSize;
-
-  const _ScanOverlay({required this.scanSize});
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      size: MediaQuery.sizeOf(context),
-      painter: _OverlayPainter(scanSize: scanSize),
-    );
-  }
-}
-
-class _OverlayPainter extends CustomPainter {
-  final double scanSize;
-
-  _OverlayPainter({required this.scanSize});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()..color = Colors.black.withAlpha(140);
-    final scanRect = Rect.fromCenter(
-      center: Offset(size.width / 2, size.height / 2),
-      width: scanSize,
-      height: scanSize,
-    );
-
-    // 绘制四周遮罩
-    canvas.drawPath(
-      Path.combine(
-        PathOperation.difference,
-        Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height)),
-        Path()..addRRect(RRect.fromRectAndRadius(scanRect, const Radius.circular(12))),
-      ),
-      paint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-/// 扫描框四角装饰
-class _CornerPainter extends CustomPainter {
-  final Color color;
-  final double cornerLength;
-  final double cornerWidth;
-
-  _CornerPainter({
-    required this.color,
-    required this.cornerLength,
-    required this.cornerWidth,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..strokeWidth = cornerWidth
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
-
-    final w = size.width;
-    final h = size.height;
-    final cl = cornerLength;
-
-    // 左上角
-    canvas.drawLine(const Offset(0, 0), Offset(cl, 0), paint);
-    canvas.drawLine(const Offset(0, 0), Offset(0, cl), paint);
-
-    // 右上角
-    canvas.drawLine(Offset(w, 0), Offset(w - cl, 0), paint);
-    canvas.drawLine(Offset(w, 0), Offset(w, cl), paint);
-
-    // 左下角
-    canvas.drawLine(Offset(0, h), Offset(cl, h), paint);
-    canvas.drawLine(Offset(0, h), Offset(0, h - cl), paint);
-
-    // 右下角
-    canvas.drawLine(Offset(w, h), Offset(w - cl, h), paint);
-    canvas.drawLine(Offset(w, h), Offset(w, h - cl), paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-/// 微信风格扫描线动画
+/// 全屏扫描线动画
 class _ScanLineAnimation extends StatefulWidget {
   const _ScanLineAnimation();
 
